105e44f6e548f8f87e17289bca894139bdfe3659
Tom Feron <tho.feron@gmail.com>
2024-05-06 18:32:00 +0100

    C++ setup, WIP

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// #include <boost/numeric/ublas/vector.hpp>
+// #include <boost/numeric/ublas/matrix.hpp>
+
+double volatility_surface() {
+}
+
+int main(int argc, char *argv[]) {
+  std::cout << "Hello, World!" << std::endl;
+
+  return 0;
+}
